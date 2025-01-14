3a45f85cdade591647c9fe166d4ae86eae6f0fba
Daniel <danielarta1992@gmail.com>
2025-01-14 00:56:17 -0500

    BACKEND aplicacion sessions maneja la logica de negoio encargada del login de la sesion del usuario

--- eterna_microservices/auth_service/sessions/views.py
+++ eterna_microservices/auth_service/sessions/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+# sessions/views.py
 
-# Create your views here.
+from django.http import JsonResponse
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.contrib.auth.models import User
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return JsonResponse({'message': 'Login successful'})
+        else:
+            return JsonResponse({'error': 'Invalid credentials'}, status=400)
+    return JsonResponse({'error': 'Invalid method'}, status=405)
+
+def logout(request):
+    auth_logout(request)
+    return JsonResponse({'message': 'Logout successful'})
