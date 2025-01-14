06f0d8bdb05206408cb8e5fbe75e3c844058799b
Daniel <danielarta1992@gmail.com>
2025-01-14 00:37:17 -0500

    BACKEND se crea el microservicio en Django para el manejo de la autenticacion y creacion de cuentas de usuario en el proyecto, integrando apirestframework, postgresql, postman, mediante lenguaje python, se configura el microservicio general y las urls generales del microservicio de manera modular para delegar sus funciones a cada aplicacion del microservicio

--- eterna_microservices/auth_service/auth_service/urls.py
+++ eterna_microservices/auth_service/auth_service/urls.py
@@ -4,6 +4,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
@@ -11,4 +17,8 @@ urlpatterns = [
     path('sessions/', include('sessions.urls')),
     path('password_reset/', include('password_reset.urls')),
     path('two_factor_auth/', include('two_factor_auth.urls')),
+    # Rutas para pruebas del microservicio
+    # path('api/auth/', include('auth_service.api.urls')),  # Nuevas rutas para probar directamente el auth_service
+    path('api/', include('auth_service.api.urls')),
+
 ]
