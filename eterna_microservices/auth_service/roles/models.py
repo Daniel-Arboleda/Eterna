df64a2378d186422f5862fa5b04b777743fa3d14
Daniel <danielarta1992@gmail.com>
2025-02-02 00:26:11 -0500

    BACKEND se actualiza la logica de negocio para el manejo de roles

--- eterna_microservices/auth_service/roles/models.py
+++ eterna_microservices/auth_service/roles/models.py
@@ -1,9 +1,20 @@
-# roles/models.py
-
 from django.db import models
+from django.conf import settings  # Para obtener el modelo de usuario
 
 class Role(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)  # Puede ser opcional
 
     def __str__(self):
         return self.name
+
+class UserRole(models.Model):
+    """
+    Modelo intermedio que asocia un usuario con un rol específico.
+    Permite que un usuario tenga múltiples roles en caso de ser necesario.
+    """
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.email} - {self.role.name}"
