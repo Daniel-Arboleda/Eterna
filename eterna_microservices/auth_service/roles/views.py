e4f9b0c968d8690e8245360d1cdccf1b27f857c7
Daniel <danielarta1992@gmail.com>
2025-02-05 02:57:39 -0500

    BACKEND ahora los roles quedan saeados para que solo el usuario logeado pueda revisar su informacion haciendo que el sistema bloquee la consulta de informacion a otros usuarios al comparar los tokens JWT

--- eterna_microservices/auth_service/roles/views.py
+++ eterna_microservices/auth_service/roles/views.py
@@ -1,6 +1,8 @@
 # roles/views.py
 
 from django.http import JsonResponse
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from .models import Role  # Asumiendo que tienes un modelo Role
 
 def create_role(request):
@@ -14,8 +16,13 @@ def create_role(request):
         return JsonResponse({'error': 'Role name is required'}, status=400)
     return JsonResponse({'error': 'Invalid method'}, status=405)
 
+
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def list_roles(request):
-    # Lógica para listar roles
-    roles = Role.objects.all()  # Suponiendo que tienes un modelo Role
-    roles_list = [{'id': role.id, 'name': role.name} for role in roles]
+    # Obtener los roles del usuario autenticado
+    user_roles = request.user.roles.all()
+    roles_list = [{'id': role.id, 'name': role.name} for role in user_roles]
     return JsonResponse({'roles': roles_list})
